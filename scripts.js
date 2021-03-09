f6af357352b155ec3291a96d3b19a1505805a2f8
Caroline S Fonseca <carolinefonseca@gec.inatel.br>
2021-03-08 21:35:08 -0300

    Adicionando os dados

--- scripts.js
+++ scripts.js
@@ -13,36 +13,42 @@ const Modal = {
   },
 }
 
-const transactions = [{
-  id: 1,
-  description: 'Luz',
-  amount: -50000,
-  date:'23/01/2021',
-
-  },
-  {
-  id: 2,
-  description: 'Website',
-  amount: 500000,
-  date:'23/01/2021',
-
-  },
-  {
-  id: 3,
-  description: 'Internet',
-  amount: -20000,
-  date:'23/01/2021',
-
-  },
-]
-
 const Transaction = {
 
-  all: transactions,
+  all: [{
+  
+    description: 'Luz',
+    amount: -50000,
+    date:'23/01/2021',
+  
+    },
+    {
+    description: 'Website',
+    amount: 500000,
+    date:'23/01/2021',
+  
+    },
+    {
+    description: 'Internet',
+    amount: -20000,
+    date:'23/01/2021',
+  
+    },
+  ],
+
+  //adicionar transações
   add(transaction){
     Transaction.all.push(transaction)
 
     App.reload()
+  },
+  
+//remover transações
+  remove(index){
+    //slipce() => função para remover de acordo com o indice e a quantidade
+    //a partir do indice
+    Transaction.all.splice(index,1);
+    App.reload();
   },
   //somar as entradas
   incomes(){
@@ -123,6 +129,18 @@ const DOM = {
 }
 
 const Utils = {
+  
+  formatAmount(value){
+    value = Number(value) *100;
+    return value;
+  },
+
+  formatDate(date){
+    const splittedDate = date.split("-")
+  //retornando no padrão dd/mm/yyyy
+    return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
+  },
+
   formatCurrency(value){
   //  Lógica para sinais negativos
     const signal = Number(value) < 0 ? "-":"";
@@ -135,6 +153,75 @@ const Utils = {
     })
     return signal+value
 
+  },
+
+}
+
+const Form = {
+  //linkando o JS com o HTML
+  description:document.querySelector('input#description'),
+  amount:document.querySelector('input#amount'),
+  date:document.querySelector('input#date'),
+
+  //guardando os valores
+  getValues(){
+    return{
+      description: Form.description.value,
+      amount: Form.amount.value,
+      date:Form.date.value,
+    }
+  },
+
+  validateFields(){
+    const {description, amount, date} = Form.getValues();
+    if(description.trim() === "" || amount.trim() ===""|| date.trim() ===""){
+      throw new Error("Preencha todos os campos");
+    }
+  },
+
+  formatValues(){
+    let {description, amount, date} = Form.getValues();
+    //formatando as var recebidas
+    amount = Utils.formatAmount(amount);
+    date = Utils.formatDate(date);
+
+    return{
+      description,
+      amount,
+      date
+    }
+  },
+
+  saveTransaction(transaction){
+    Transaction.add(transaction);
+  },
+
+  clearFields(){
+
+    Form.description.value = "";
+    Form.amount.value = "";
+    Form.date.value = "";
+  },
+  
+  submit(event){
+    event.preventDefault();
+
+    try {
+    // verificando se todas as infos foram preenchidas
+      Form.validateFields();
+    //formatando os dados
+      const transaction = Form.formatValues();
+    //salvando
+      Form.saveTransaction(transaction);
+    //apagando os dados
+      Form.clearFields();
+    //fechando modal
+      Modal.close();
+
+    } catch (error) {
+      alert(error.message);
+    }
+    
   }
 }
 
@@ -156,9 +243,3 @@ const App = {
 
 App.init();
 
-Transaction.add({
-  id:39,
-  description:'Faxina',
-  amount: -7000,
-  date:'05/03/2021'
-})
